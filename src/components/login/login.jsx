0f3d9f907c5df9e57a8943d3b491e9054fffa6cd
harshith-sj <harshithsj03@gmail.com>
2024-12-06 21:37:37 +0530

    added toastify

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,47 +1,55 @@
-import "./login.css"
-import { useState } from "react"
+import React, { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./login.css";
 
 const Login = () => {
-    const [avatar,setAvatar] = useState({
-        file:null,
-        url:""
-    })
+  const [avatar, setAvatar] = useState({
+    file: null,
+    url: ""
+  });
 
-    const handleAvatar = (e) => {
-        if(e.target.files[0]){
-        setAvatar({
-            file:e.target.files[0],
-            url:URL.createObjectURL(e.target.files[0])
-        })
+  const handleAvatar = (e) => {
+    if (e.target.files[0]) {
+      setAvatar({
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0])
+      });
     }
-}
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div className='login'>
-        <div className="item">
-            <h2>Welcome back</h2>
-            <form >
-                <input type="text" placeholder="Email" name="email"/>
-                <input type="password" placeholder="Password" name="password"/>
-               <button>Sign In</button>
-            </form>
-        </div>
-        <div className="separator"></div>
-        <div className="item">
+      <div className="item">
+        <h2>Welcome back</h2>
+        <form onSubmit={handleLogin}>
+          <input type="text" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <button type="submit">Sign In</button>
+        </form>
+      </div>
+      <ToastContainer />
+      <div className="separator"></div>
+      <div className="item">
         <h2>Create an Account</h2>
-            <form >
-                <label htmlFor="file">
-                    <img src={avatar.url || "./avatar.png"} alt="" />
-                Upload the image</label>
-                <input type="file" id="file" style={{display:"none"}} onChange={handleAvatar}/>
-                <input type="text"  placeholder="username" name="username"/>
-                <input type="text" placeholder="Email" name="email"/>
-                <input type="password" placeholder="Password" name="password"/>
-               <button>Sign In</button>
-            </form>
-        </div>
+        <form>
+          <label htmlFor="file">
+            <img src={avatar.url || "./avatar.png"} alt="" />
+            Upload the image
+          </label>
+          <input type="file" id="file" style={{ display: "none" }} onChange={handleAvatar} />
+          <input type="text" placeholder="username" name="username" />
+          <input type="text" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <button>Sign In</button>
+        </form>
+      </div>
     </div>
   );
 };
 
-export default Login
+export default Login;
